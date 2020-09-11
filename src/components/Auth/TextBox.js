370347904cdde9d0534a2b08a645a15d0e7c2d23
Johnsavadkuhi <physitime@gmail.com>
2020-09-12 01:35:06 +0430

    add error

--- src/components/Auth/TextBox.js
+++ src/components/Auth/TextBox.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Label, Input } from "reactstrap";
+import { Label, Input, FormText } from "reactstrap";
 
 function TextBox(props) {
-  
-  const {iconSrc , ...restProps} = props ; 
+
+  const { iconSrc, error, hasError, ...restProps } = props;
 
   return (
     <>
       <div className="inputs-text">
         <img src={iconSrc} alt="parallex img" />
         <Label htmlFor={props.type === "email" ? "email" : "password"}></Label>
-        <Input
-        {...restProps}
-/>
-      </div><span style={{padding:0  , marginLeft:"10px"  , top : 0    }}>  fsds</span>
+        <Input valid={hasError} invalid={hasError}
+          {...restProps}
+        />
+      </div>{hasError ? <FormText >{error}</FormText> : <></>}
+
     </>
   );
 }
