bd415a89941b227dbc4963407dc2b845db4e1048
Johnsavadkuhi <physitime@gmail.com>
2020-09-11 23:54:00 +0430

    add ErrorBoundary

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Auth from '.'; 
+import Auth from './Auth';
+import ErrorBoundary from './Error';
 
 function App() {
 
-  return <>
-  <Auth/>
-  </>
+  return <ErrorBoundary>
+    <Auth />
+  </ErrorBoundary>
 }
 
 export default App;
