2b6313a91a8203cc723276dded6dd62abcbf3205
Johnsavadkuhi <physitime@gmail.com>
2020-09-14 22:30:40 +0430

    update

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form  } from "reactstrap";
+import { Form } from "reactstrap";
 import TextBox from "./TextBox";
 import Submit from "./Submit";
 import { useForm } from "react-hook-form";
@@ -7,32 +7,37 @@ import iconSrc from "../../Assets/icons/email.svg";
 import passIconSrc from "../../Assets/icons/password.svg";
 import userIconSrc from "../../Assets/icons/user.svg"
 import { useFormik } from "formik";
-import * as Yup from 'yup' ; 
+import * as Yup from 'yup';
 
 
- function SignUp() {
+function SignUp() {
 
-  
+
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = data => console.log("data : " , data );
+  const onSubmit = data => console.log("data : ", data);
 
   const formik = useFormik({
-    initialValues : {
-      firstname : '' , lastname : '' , email : '' , password : '' 
-        } , 
-        validationSchema : Yup.object({
-          firstname : Yup.string().required("Required") , 
-          lastname : Yup.string().required("Required"), 
-          email : Yup.string().email("Invalid email address")
-          .required("Required")
-        }), 
-        onSubmit : values => {
-          
-        }
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: ''
+
+    },
+
+    validationSchema: Yup.object({
+      firstname: Yup.string().required("Required"),
+      lastname: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address")
+        .required("Required")
+    }),
+    onSubmit: values => {
+
+    }
   })
+
   // const textBoxProps = (which) => {
-    
   //   if(which === "firstname" ) {
   //     return {
   //       id : "firstname" ,
@@ -76,69 +81,69 @@ import * as Yup from 'yup' ;
   // };
 
   return (
-     
-      <Form className="sign-up" onSubmit={formik.handleSubmit}>
-        
-        <TextBox 
-        id = "firstname" 
-        iconSrc = {userIconSrc }
-        type="text" 
-        placeholder = "First Name "
-        name= "firstname"
+
+    <Form className="sign-up" onSubmit={formik.handleSubmit}>
+
+      <TextBox
+        id="firstname"
+        iconSrc={userIconSrc}
+        type="text"
+        placeholder="First Name "
+        name="firstname"
         className="input-text"
-        onchange ={formik.handleChange}
-        onBlur = {formik.handleBlur}
-        value = {formik.values.firstname}
+        onchange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstname}
 
-        />
+      />
 
-        <TextBox 
-          iconSrc = {userIconSrc } 
-          type="text"  
-          id="lastname"
-          placeholder = "Last Name "
-          name="lastname"
-          className="input-text"
-          onchange= {formik.handleChange}
-          onBlur ={formik.handleBlur}
-          value={formik.values.lastname}
+      <TextBox
+        iconSrc={userIconSrc}
+        type="text"
+        id="lastname"
+        placeholder="Last Name "
+        name="lastname"
+        className="input-text"
+        onchange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastname}
 
-        />
+      />
 
-        <TextBox 
-        iconSrc
+      <TextBox
+        iconSrc={iconSrc}
         type="email"
         id="email"
         placeholder="Email"
-        name= "email"
-        className= "input-text"
+        name="email"
+        className="input-text"
         onchange={formik.handleChange}
         onBlur={formik.handleBlur}
-        value ={formik.values.email}
+        value={formik.values.email}
 
-        />
+      />
 
-        <TextBox 
-        iconSrc = {passIconSrc}
+      <TextBox
+        iconSrc={passIconSrc}
         type="password"
         id="password"
-        placeholder= "Password"
-        name= "password"
-        className= "input-text"
+        placeholder="Password"
+        name="password"
+        className="input-text"
         onchange={formik.handleChange}
         onBlur={formik.handleBlur}
         value={formik.values.password}
-        />
+      />
 
-       <Submit children="Sign Up " />
+      <Submit children="Sign Up " />
 
-      
-      </Form>
-  
+
+    </Form>
+
   );
 
 
 
 }
 
-export default SignUp ; 
+export default SignUp; 
