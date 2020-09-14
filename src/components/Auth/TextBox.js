07fbce547f811a4ccad5f27b9a02ad4f15fa2823
Johnsavadkuhi <physitime@gmail.com>
2020-09-14 22:41:21 +0430

    update

--- src/components/Auth/TextBox.js
+++ src/components/Auth/TextBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  Input, FormText } from "reactstrap";
+import { Input, FormText } from "reactstrap";
 
 function TextBox(props) {
 
@@ -8,12 +8,14 @@ function TextBox(props) {
   return (
     <>
       <div className="inputs-text">
+
         <img src={iconSrc} alt="parallex img" />
-        {/* <Label htmlFor={props.type === "email" ? "email" : "password"}></Label> */}
-        <Input valid={hasError} invalid={hasError}
-          {...restProps}
-        />
-      </div>{hasError ? <FormText >{error}</FormText> : <></>}
+
+        <Input valid={hasError} invalid={hasError} {...restProps} />
+
+      </div>
+
+      {hasError && <FormText >{error}</FormText>}
 
     </>
   );
