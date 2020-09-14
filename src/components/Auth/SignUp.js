9881911b67a1d2b4cc5983b824a612d9219f66a9
Johnsavadkuhi <physitime@gmail.com>
2020-09-14 22:25:55 +0430

    use formik

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -6,67 +6,126 @@ import { useForm } from "react-hook-form";
 import iconSrc from "../../Assets/icons/email.svg";
 import passIconSrc from "../../Assets/icons/password.svg";
 import userIconSrc from "../../Assets/icons/user.svg"
+import { useFormik } from "formik";
+import * as Yup from 'yup' ; 
+
 
  function SignUp() {
 
+  
   const { register, handleSubmit } = useForm();
 
   const onSubmit = data => console.log("data : " , data );
 
-
-  const textBoxProps = (which) => {
+  const formik = useFormik({
+    initialValues : {
+      firstname : '' , lastname : '' , email : '' , password : '' 
+        } , 
+        validationSchema : Yup.object({
+          firstname : Yup.string().required("Required") , 
+          lastname : Yup.string().required("Required"), 
+          email : Yup.string().email("Invalid email address")
+          .required("Required")
+        }), 
+        onSubmit : values => {
+          
+        }
+  })
+  // const textBoxProps = (which) => {
     
-    if(which === "firstname" ) {
-      return {
-        innerRef : register({require:false}),
-        iconSrc : userIconSrc , 
-        type:"text" , 
-        placeholder : "First Name ",
-        name:"firstName", 
-        className:"input-text"
-      }
-    } 
-    if(which==="lastname") {
-      return {
-        innerRef : register({require:false}),
-        iconSrc : userIconSrc , 
-        type:"text" , 
-        placeholder : "Last Name ",
-        name:"lastname", 
-        className:"input-text"
-      }
-    }
-    if (which === "email")
-      return {
-        innerRef:register({required:true}) , 
-        iconSrc,
-        type: "email",
-        placeholder: "Email",
-        name: "email",
-        className: "input-text",
-      } 
-      return {
-        innerRef : register({required:true}), 
-        iconSrc: passIconSrc,
-        type: "password",
-        placeholder: "Password",
-        name: "password",
-        className: "input-text",
-      }
+  //   if(which === "firstname" ) {
+  //     return {
+  //       id : "firstname" ,
+  //       innerRef : register({require:false}),
+  //       iconSrc : userIconSrc , 
+  //       type:"text" , 
+  //       placeholder : "First Name ",
+  //       name:"firstname", 
+  //       className:"input-text", 
+  //       onchange :{formik.handleChange}
+  //     }
+  //   } 
+  //   if(which==="lastname") {
+  //     return {
+  //       innerRef : register({require:false}),
+  //       iconSrc : userIconSrc , 
+  //       type:"text" , 
+  //       placeholder : "Last Name ",
+  //       name:"lastname", 
+  //       className:"input-text"
+  //     }
+  //   }
+  //   if (which === "email")
+  //     return {
+  //       innerRef:register({required:true}) , 
+  //       iconSrc,
+  //       type: "email",
+  //       placeholder: "Email",
+  //       name: "email",
+  //       className: "input-text",
+  //     } 
+  //     return {
+  //       innerRef : register({required:true}), 
+  //       iconSrc: passIconSrc,
+  //       type: "password",
+  //       placeholder: "Password",
+  //       name: "password",
+  //       className: "input-text",
+  //     }
 
-  };
+  // };
 
   return (
      
-      <Form className="sign-up" onSubmit={handleSubmit(onSubmit)}>
+      <Form className="sign-up" onSubmit={formik.handleSubmit}>
         
-        <TextBox {...textBoxProps("firstname")}/>
+        <TextBox 
+        id = "firstname" 
+        iconSrc = {userIconSrc }
+        type="text" 
+        placeholder = "First Name "
+        name= "firstname"
+        className="input-text"
+        onchange ={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        value = {formik.values.firstname}
 
-        <TextBox {...textBoxProps("lastname")} />
+        />
 
-        <TextBox {...textBoxProps("email")} />
+        <TextBox 
+          iconSrc = {userIconSrc } 
+          type="text"  
+          placeholder = "Last Name "
+          name="lastname"
+          className="input-text"
+          onchange= {formik.handleChange}
+          onBlur ={formik.handleBlur}
+          value={formik.values.lastname}
 
-        <TextBox {...textBoxProps("password")} />
+        />
+
+        <TextBox 
+        iconSrc
+        type="email"
+        placeholder="Email"
+        name= "email"
+        className= "input-text"
+        onchange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value ={formik.values.email}
+
+        />
+
+        <TextBox 
+        iconSrc = {passIconSrc}
+        type="password"
+        placeholder= "Password"
+        name= "password"
+        className= "input-text"
+        onchange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+        />
 
        <Submit children="Sign Up " />
 
