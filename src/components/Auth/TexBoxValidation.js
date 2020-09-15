485069e8ea2ce24f48ba0835bda8cf761b0b0e2a
Johnsavadkuhi <physitime@gmail.com>
2020-09-15 16:51:27 +0430

    update

--- src/components/Auth/TexBoxValidation.js
+++ src/components/Auth/TexBoxValidation.js
@@ -1,49 +1,49 @@
-import React from 'react'; 
+import React from 'react';
 
-function TextBoxValidation (props){
+function TextBoxValidation(props) {
 
-    const {formik , name } =props; 
+    const { formik, name } = props;
 
-    if(name === "firstname"){
+    if (name === "firstname") {
         return <>
-   
-        {formik.touched.firstname && formik.errors.firstname ? (
-             <div style={{color:"red"}}>{formik.errors.firstname}</div>
-           ) : null}
-        
+
+            {formik.touched.firstname && formik.errors.firstname ? (
+                <div style={{ color: "red" }}>{formik.errors.firstname}</div>
+            ) : null}
+
         </>
 
     }
-    if(name==="lastname"){
+    if (name === "lastname") {
         return <>
-   
-        {formik.touched.lastname && formik.errors.lastname ? (
-             <div style={{color:"red"}}>{formik.errors.lastname}</div>
-           ) : null}
-        
+
+            {formik.touched.lastname && formik.errors.lastname ? (
+                <div style={{ color: "red" }}>{formik.errors.lastname}</div>
+            ) : null}
+
         </>
     }
-   if(name==="email") {
-       
-    return <>
-   
-    {formik.touched.email && formik.errors.email ? (
-         <div style={{color:"red"}}>{formik.errors.email}</div>
-       ) : null}
-    
-    </>
-   }
-   if(name==="password"){
-    return <>
-   
-    {formik.touched.password && formik.errors.password ? (
-         <div style={{color:"red"}}>{formik.errors.password}</div>
-       ) : null}
-    
-    </>
-   }
+    if (name === "email") {
+
+        return <>
+
+            {formik.touched.email && formik.errors.email ? (
+                <div style={{ color: "red" }}>{formik.errors.email}</div>
+            ) : null}
+
+        </>
+    }
+    if (name === "password") {
+        return <>
+
+            {formik.touched.password && formik.errors.password ? (
+                <div style={{ color: "red" }}>{formik.errors.password}</div>
+            ) : null}
+
+        </>
+    }
 
 
 }
 
-export default TextBoxValidation ; 
+export default TextBoxValidation; 
